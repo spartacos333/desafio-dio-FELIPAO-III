6568372f4c77394e143a98520329f9b435ee5a2b
spartacos333 <147752167+spartacos333@users.noreply.github.com>
2023-11-03 11:16:10 -0300

    criei a pasta SRC
    SRC : pasta contendo todos os testes e a versao final do codigo.

---
+++ SRC/teste00.js
@@ -0,0 +1,17 @@
+// Pretendo simular uma rodada de ataque de uma equipe em um jogo de RPG.
+// Nesse teste, meu objetivo principal foi criar um código funcional com as bases para a ideia inicial.
+// Para o próximo teste, pretendo codificar em inglês.
+let hero = {
+    name : "Spartacos333",
+    age : 10,
+    class : Mage = {
+        ataque : "Magia"
+    }
+  };
+  console.log ( `O ${hero.name} atacou usando ${hero.class.ataque} de class !`);
+ hero.localizacao = "Salao principal";
+  console.log ( `${hero.name}  está no ${hero.localizacao} .`);
+/*  Saída:
+  O Spartacos333 atacou usando Magia de class !
+  Spartacos333  está no Salao principal .
+*/
